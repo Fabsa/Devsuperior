7837b6984f9f743b60c519ae59b598fe12b3c5f4
Fabio Araujo <fabsan02@outlook.com>
2021-05-08 16:28:21 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,37 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+
+
 const DashBoard = () => {
     return (
-     
-        <h1>Pagina Dasborad</h1>
+        <>
+        <NavBar />
+        <div className="container">
+  
+          <h1 className="text-primary px-3">DashBoard de Vendas</h1>
+          <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secodary">Taxa de sucesso (%)</h5>
+              <BarChart />
+            </div>
+            <div className="col-sm-6">
+              <h5 className="text-center text-secodary">Todas vendas</h5>
+              <DonutChart />
+            </div>
+  
+          </div>
+          <div className="py-3">
+           <h2 className="text-primary">Todas vendas</h2>
+          </div>
+          <DataTable />
+        </div>
+        <Footer />
+      </>
+
       
     );
   }
