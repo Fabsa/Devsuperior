7b6a2922cfd8a7909cd8102263e1cc9d02f936ec
Fabio Araujo <fabsan02@outlook.com>
2021-05-09 01:20:50 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL?? 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 //define o caminho da variavel ambiente criada no heroku...
