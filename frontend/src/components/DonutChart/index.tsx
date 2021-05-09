bb8c189548ea021b5186f1b46afb514320253db0
Fabio Araujo <fabsan02@outlook.com>
2021-05-08 22:04:39 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,11 +24,7 @@ const DonutChart = () => {//função sem nome
 
             });
     }, []);
-    /*const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }*/
-
+   
     const options = {
         legend: {
             show: true
